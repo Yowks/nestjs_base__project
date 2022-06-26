1697064a9517b320efdfb0902143ae31c331ed4e
Alex <fernandesalexandre1996@gmail.com>
2022-06-26 19:14:55 +0200

    Base modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthenticationModule } from './authentication/authentication.module';
+import { CatalogModule } from './catalog/catalog.module';
+import { CartModule } from './cart/cart.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [AuthenticationModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type :"sqlite",
+      database: "ecommerce",
+      entities: [__dirname + "/**/*.entity{.ts,.js}"],
+      synchronize: true
+    }),
+    AuthenticationModule, 
+    CatalogModule, 
+    CartModule,
+    UserModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
