1bf5e2186687dcc75d134eade6827a694e61a276
Alex <fernandesalexandre1996@gmail.com>
2022-06-26 22:09:22 +0200

    Export cart module updated with controller, service and entity

--- src/cart/cart.module.ts
+++ src/cart/cart.module.ts
@@ -1,4 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Cart } from './cart.entity';
+import { CartController } from './controller/cart.controller';
+import { CartService } from './service/cart.service';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([Cart])],
+  controllers: [CartController],
+  providers: [CartService]
+})
 export class CartModule {}
