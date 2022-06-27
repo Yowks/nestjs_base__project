72d03b3fc15baa756315a9731bee5fcc2b5c07b5
Alex <fernandesalexandre1996@gmail.com>
2022-06-27 21:38:52 +0200

    Clean DB

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Ecommerce')
+    .setDescription('The ecommerce API description')
+    .setVersion('1.0')
+    .addTag('ecommerce')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
